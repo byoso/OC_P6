e1d4c2abdfb27360ec440c38d1d6cb60f22c218c
byoso <emailvinz@gmail.com>
2021-11-28 16:40:16 +0100

    searching

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+let server_domain = "http://localhost:8000/"
+
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
 
 const space_best = document.getElementById("space_best");
 let space_bests = document.getElementById("space_bests");
@@ -7,10 +18,9 @@ let space_sci_fi = document.getElementById("space_sci_fi");
 console.log("space best : "+space_bests)
 
 
-// getData("http://localhost:8000/api/v1/titles/", space_western)
-getData("http://localhost:8000/api/v1/titles/?genre=Western&sort_by=-imdb_score", space_western)
-getData("http://localhost:8000/api/v1/titles/?genre=Sci-fi&sort_by=-imdb_score", space_sci_fi)
-getData("http://localhost:8000/api/v1/titles/?genre=Comedy&sort_by=-imdb_score", space_comedy)
+getData(server_domain+"api/v1/titles/?genre=Western&sort_by=-imdb_score", space_western)
+getData(server_domain+"api/v1/titles/?genre=Sci-fi&sort_by=-imdb_score", space_sci_fi)
+getData(server_domain+"api/v1/titles/?genre=Comedy&sort_by=-imdb_score", space_comedy)
 
 
 
@@ -26,9 +36,11 @@ function builder(elems, space){
 function getData(url, space) {
     fetch(url)
     .then(function(response) {
-      return response.json();
+        console.log(`response : ${response}`);
+        return response.json();
     })
     .then(function(json) {
+        console.log(`json : ${json}`);
         let elems = json.results
         builder(elems, space)
     });

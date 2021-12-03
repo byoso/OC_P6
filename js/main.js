b8b4aab98e895aaa9232f67b4d752c419118d616
byoso <emailvinz@gmail.com>
2021-12-03 15:50:38 +0100

    some code cleaning

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-let server_domain = "http://localhost:8000/"
+let domain_name = "http://localhost:8000/"
 let elem_id = 0
 let films_elems = []
 let dom_elems = []
@@ -19,22 +19,20 @@ const space_western = document.getElementById("space_western");
 const space_sci_fi = document.getElementById("space_sci_fi");
 
 
-let best = getData(server_domain+"api/v1/titles/?sort_by=-imdb_score", space_best, best_num)
-let bests = getData(server_domain+"api/v1/titles/?sort_by=-imdb_score", space_bests, bests_num)
-let westerns = getData(server_domain+"api/v1/titles/?genre=Western&sort_by=-imdb_score", space_western, western_num)
-let sci_fis = getData(server_domain+"api/v1/titles/?genre=Sci-fi&sort_by=-imdb_score", space_sci_fi, sci_fi_num)
-let comedies = getData(server_domain+"api/v1/titles/?genre=Comedy&sort_by=-imdb_score", space_comedy, comedy_num)
+let best = getData(domain_name+"api/v1/titles/?sort_by=-imdb_score", space_best, best_num, true)
+let bests = getData(domain_name+"api/v1/titles/?sort_by=-imdb_score", space_bests, bests_num)
+let westerns = getData(domain_name+"api/v1/titles/?genre=Western&sort_by=-imdb_score", space_western, western_num)
+let sci_fis = getData(domain_name+"api/v1/titles/?genre=Sci-fi&sort_by=-imdb_score", space_sci_fi, sci_fi_num)
+let comedies = getData(domain_name+"api/v1/titles/?genre=Comedy&sort_by=-imdb_score", space_comedy, comedy_num)
 
 
-function getData(url, space, nbre) {
+
+function getData(url, space, nbre, first=false) {
     let elems = []
     fetch(url)
     .then(function(response) {
         // console.log(`response : ${response}`);
-        return response;
-    })
-    .then(function(response){
-        return response.json()
+        return response.json();
     })
     .then(function(json){
         let data = json
@@ -53,7 +51,11 @@ function getData(url, space, nbre) {
                 return elems
             })
             .then(function(elems){
-                builder(elems, space)
+                if (first){
+                    buildFirst(elems, space)
+                }else{
+                    builder(elems, space)
+                }
             })
         };
     })
@@ -81,13 +83,37 @@ function builder(elems, space){
     }
 };
 
+function buildFirst(elems, space){
+    console.log("build the best")
+    for(let elem of elems){
+        elem_id ++
+        console.log(elem);
+        let dom_elem = document.createElement("div");
+        dom_elem.classList.add('best')
+        dom_elem.id = `filmId${elem_id}`
+        films_elems.push(elem.id)
+        dom_elems.push(dom_elem)
+        dom_elem.innerHTML = `
+        <div>
+            <h2>${elem.title}</h2>
+            <button class="play">Play</button>
+        </div>
+        <img src="${elem.image_url}" alt="cover">
+
+        `
+        ;
+        space.appendChild(dom_elem);
+        pageLoaded()
+    }
+}
+
 
 function pageLoaded(){
     if (films_elems.length < total_elems){
         // console.log("loading...")
     }else {
-        // console.log("loaded")
-        for(let i=0; films_elems[i];i++){
+        // Chaque element devient clikable sauf le premier (meilleur film)
+        for(let i=1; films_elems[i];i++){
             dom_elems[i].onclick = function(){
                 modalBuilder(films_elems[i])
             }
@@ -98,7 +124,7 @@ function pageLoaded(){
 
 function modalBuilder(film_id){
     console.log("build modal for ", film_id);
-    fetch(server_domain+"api/v1/titles/"+film_id)
+    fetch(domain_name+"api/v1/titles/"+film_id)
     .then(function(response) {
         // console.log(`response : ${response}`);
         return response;
@@ -120,8 +146,6 @@ function modalBuilder(film_id){
 function modalSetting(elem){
     // Get the modal
     var modal = document.getElementById("modalWindow");
-    // Get the button that opens the modal
-    // var btn = document.getElementById(`filmId${elem_id}`);
     // Get the <span> element that closes the modal
     var span = document.getElementById("closeModal");
     // When the user clicks on <span> (x), close the modal
@@ -159,11 +183,12 @@ function modalSetting(elem){
     `
 };
 
+
 // carousels
 
 function slideRight(id){
     var space = document.getElementById(id)
-    space.classList.remove("left")
+    space.classList.remove("left");
     space.classList.add("right");
 }
 
